bca766c2b6aa06ae795261a562a52fe1a5846f4e
samra-xloop <samra.almas@xloopdigital.com>
2022-12-06 17:42:21 +0500

    st2 AC passed(changed code inside test to loop)

--- src/Machine.js
+++ src/Machine.js
@@ -1,4 +1,4 @@
-module.exports=class Machine {
+class Machine {
     
     constructor() {
         this.listOfItems=[{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
@@ -8,5 +8,25 @@ module.exports=class Machine {
     seeSelections() {
         return this.listOfItems;
     }
+    deposit(amount){
+        switch(amount){
+            case 10: return 'you have deposited Rs 10'; 
+            break;
+            case 20: return 'you have deposited Rs 20'; 
+            break;
+            case 50: return 'you have deposited Rs 50'; 
+            break;
+            case 100: return 'you have deposited Rs 100'; 
+            break;
+            case 500: return 'you have deposited Rs 500'; 
+            break;
+            default:
+                'Please Enter 10 / 20 / 50 / 100 /500'
+        }
+    }
 };
 
+module.exports=Machine
+
+// ob=new Machine
+// console.log(ob.deposit(10))
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -16,26 +16,18 @@ describe('the vending machine', () => {
     it('should show deposited money', () => {
         // setup
         const machine = new Machine();
-        const expected1 = 'you have deposited Rs 10'
-        const expected2 = 'you have deposited Rs 20'
-        const expected3 = 'you have deposited Rs 50'
-        const expected4 = 'you have deposited Rs 100'
-        const expected5 = 'you have deposited Rs 500'
+        const amounts=[10,20,50,100,500]
 
-        // exercise
-        const actual1 = machine.deposit(10);
-        const actual2 = machine.deposit(20);
-        const actual3 = machine.deposit(50);
-        const actual4 = machine.deposit(100);
-        const actual5 = machine.deposit(500);
+        for(let i of amounts){
+            actual = machine.deposit(i);
+            expected= `you have deposited Rs ${i}`
+            expect(actual).toEqual(expected);
+        }
 
-        // assert
-        expect(actual1).toEqual(expected1);
-        expect(actual2).toEqual(expected2);
-        expect(actual3).toEqual(expected3);
-        expect(actual4).toEqual(expected4);
-        expect(actual5).toEqual(expected5);
+    
     });
+
+   
 
 
 });
