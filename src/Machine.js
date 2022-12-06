0d54add06585d72ed083b85ee462965df6235773
samra-xloop <samra.almas@xloopdigital.com>
2022-12-06 17:06:25 +0500

    story 2 AC failed

--- src/Machine.js
+++ src/Machine.js
@@ -1,12 +1,12 @@
 module.exports=class Machine {
     
     constructor() {
-       
+        this.listOfItems=[{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
     }
 
 
     seeSelections() {
-        
+        return this.listOfItems;
     }
 };
 
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -13,4 +13,29 @@ describe('the vending machine', () => {
         expect(actual).toEqual(expected);
     });
 
+    it('should show deposited money', () => {
+        // setup
+        const machine = new Machine();
+        const expected1 = 'you have deposited Rs 10'
+        const expected2 = 'you have deposited Rs 20'
+        const expected3 = 'you have deposited Rs 50'
+        const expected4 = 'you have deposited Rs 100'
+        const expected5 = 'you have deposited Rs 500'
+
+        // exercise
+        const actual1 = machine.deposit(10);
+        const actual2 = machine.deposit(20);
+        const actual3 = machine.deposit(50);
+        const actual4 = machine.deposit(100);
+        const actual5 = machine.deposit(500);
+
+        // assert
+        expect(actual1).toEqual(expected1);
+        expect(actual2).toEqual(expected2);
+        expect(actual3).toEqual(expected3);
+        expect(actual4).toEqual(expected4);
+        expect(actual5).toEqual(expected5);
+    });
+
+
 });
